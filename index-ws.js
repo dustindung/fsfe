f2aa03266eb44e5ff93db76c1ceccdeac75651c0
Dustin <dustin@Dustins-MacBook-Pro.local>
2023-11-30 09:29:20 +0700

    Create websocket

---
+++ index-ws.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const server = require("http").createServer();
+const app = express();
+
+app.get("/", function (req, res) {
+  res.sendFile("index.html", { root: __dirname });
+});
+
+server.on("request", app);
+server.listen(3000, function () {
+  console.log("server listen on port 3000");
+});
+
+/** Begin websocks */
+const WebSocketServer = require("ws").Server;
+
+const wss = new WebSocketServer({ server: server });
+
+wss.on("connection", function connection(ws) {
+  const numClients = wss.clients.size;
+  console.log("Clients connected", numClients);
+
+  wss.broadcast(`Current visitor: ${numClients}`);
+
+  if (ws.readyState === ws.OPEN) ws.send("Welcome to my server");
+
+  ws.on("close", function close() {
+    wss.broadcast(`Current visitor: ${numClients}`);
+    console.log("A client has disconnected");
+  });
+});
+
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach(function each(client) {
+    client.send(data);
+  });
+};
